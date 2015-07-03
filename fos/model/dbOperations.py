afc69a7568dbfa06e93fdf7ffdd23647db199403
jhawar_p <jhawar.pranami@gmail.com>
2015-07-03 13:09:13 +0530

    commented connection file

--- fos/model/dbOperations.py
+++ fos/model/dbOperations.py
@@ -1,22 +1,27 @@
 __author__ = 'khandave_g'
 
+# importing mongo db package for mongoclient
 from pymongo import MongoClient
 
+# creating connection with MongoClient
 def connectToDb():
     client = MongoClient()
     db = client["fosDb"]
     return db
 
+# creating customers Collection to fosDb
 def createCustomerAccount():
     db = connectToDb()
     customer = db["customers"]
     return customer
 
+# creating Admins collection to fosDb
 def createAdminAccount():
     db = connectToDb()
     admins = db["admins"]
     return admins
 
+# creating Vendors collection to fosDb
 def createVendorAccount():
     db = connectToDb()
     vendors = db["vendors"]
