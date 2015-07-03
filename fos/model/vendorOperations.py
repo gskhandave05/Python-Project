d01b1f66b678244ea910ae7eb4ebe7478be6d9c5
khandave_g <gskhandve@gmail.com>
2015-07-03 13:19:02 +0530

    - Added code for inserting vendor details to database

--- fos/model/vendorOperations.py
+++ fos/model/vendorOperations.py
@@ -2,8 +2,21 @@ __author__ = 'khandave_g'
 # This file contains methods performing operations of vendor
 
 from pymongo import MongoClient
+from dbOperations import *
 
-def createVendorAccount(name,email,contact,username,password,menu,customerReviews):
-    employees = db["vendors"]
+def insertVendor(id,name,email,contact,username,password,menu,customerReviews):
+    vendor = createVendorAccount()
+    vendorDetails = {
+        "_id":id,
+        "name":name,
+        "email":email,
+        "contact":contact,
+        "username":username,
+        "password":password,
+        "menu":menu,
+        "customerReviews":customerReviews
+    }
+
+    vendor.insert_one(vendorDetails)
 
 
