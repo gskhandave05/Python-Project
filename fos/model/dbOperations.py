8ccb1689b2b17d4b31ce9504b8de53c661664347
jhawar_p <jhawar.pranami@gmail.com>
2015-07-07 16:20:48 +0530

    updated adminOperations file

--- fos/model/dbOperations.py
+++ fos/model/dbOperations.py
@@ -18,3 +18,15 @@ def addVendor(name, contact, email, username, password, isActive, menu):
         cur.execute(
             "INSERT INTO VENDORS(name,contact,email,username, password,isActive,menu_id) VALUES (%s,%s,%s,%s,%s,%d,%d)",
             (name, contact, email,username,password,isActive, menu_id + 1))
+
+def addCustomer(name, contact, email, isRegistered, username, password, flat_no, building, street, area, city, state, pincode ):
+    con = connectToDb()
+    with con:
+        cur = con.cursor()
+        cur.execute("INSERT INTO ADDRESSES(flat_no, building, street, area, city, state, pincode) VALUES (%s,%s,%s,%s,%s,%s,%s),"
+                    (flat_no, building, street, area, city, state, pincode))
+        address_id = cur.execute("SELECT ADDRESS_ID FROM ADDRESSES ORDER BY ADDRESS_ID DESC LIMIT 1 ")
+        cur.execute("INSERT INTO CUSTOMERS(name, contact, email, address_id, isRegistered, username, password)VALUES(%s,%s,%s,%s,%s,%s,%s),"
+                    (name, contact, email, address_id, isRegistered, username, password))
+
+
